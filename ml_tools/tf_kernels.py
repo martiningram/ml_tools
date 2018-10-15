70525e71614b4019b08543f2035717d9dc53380e
Martin Ingram <ingramm@student.unimelb.edu.au>
2018-10-15 14:20:42 +1100

    Overhaul of kernel computation
    Inspired by GPFlow. Hopefully correct -- needs testing!

--- ml_tools/tf_kernels.py
+++ ml_tools/tf_kernels.py
@@ -34,6 +34,8 @@ def ard_rbf_kernel(x1, x2, lengthscales, alpha, jitter=1e-5):
 
 def ard_rbf_kernel_batch(x1, x2, lengthscales, alpha, jitter=1e-5):
 
+    # TODO: This needs testing!
+
     # x1 is N1 x D
     # x2 is N2 x D (and N1 can be equal to N2)
     # lengthscales is B x D [B is batch dim]
@@ -45,22 +47,31 @@ def ard_rbf_kernel_batch(x1, x2, lengthscales, alpha, jitter=1e-5):
     # Also must match lengthscales
     assert(lengthscales.get_shape()[1] == x1.get_shape()[1])
 
-    # Use broadcasting
-    # X1 will be (N1, 1, D)
-    x1_expanded = tf.expand_dims(x1, axis=1)
-    # X2 will be (1, N2, D)
-    x2_expanded = tf.expand_dims(x2, axis=0)
+    l_expanded = tf.expand_dims(lengthscales, axis=1)
 
-    # These will be B x N1 x N2 x D
-    scaled_diffs = (tf.expand_dims(x1_expanded - x2_expanded, axis=0) /
-                    tf.expand_dims(tf.expand_dims(lengthscales, axis=1),
-                                   axis=1))**2
+    # Divide x1 by lengthscales
+    # Gives (B x N x D)
+    x1 = x1 / l_expanded
+    x2 = x2 / l_expanded
 
-    # Use broadcasting to do a dot product
-    exponent = tf.reduce_sum(scaled_diffs, axis=3)
+    # This will be (B x N)
+    x1s = tf.reduce_sum(tf.square(x1), axis=2)
+    x2s = tf.reduce_sum(tf.square(x2), axis=2)
 
+    # This produces an (N x N) matrix
+    cross_prods = -2 * tf.matmul(x1, x2, transpose_b=True)
+
+    # This should produce a (B x N x N) distance mat
+    # Expanding x1s produces (B x 1 x N), which means it is subtracted from each
+    # row.
+    # Expanding x2s produces (B x N x 1), which means it is subtracted from each
+    # column.
+    dist = (cross_prods + tf.expand_dims(x1s, axis=1) +
+            tf.expand_dims(x2s, axis=2))
+
+    # Multiply all of these
     kern = (tf.expand_dims(tf.expand_dims(alpha**2, axis=1), axis=1) *
-            tf.exp(-0.5 * exponent))
+            tf.exp(-0.5 * dist))
 
     # Jitter this a little bit
     kern = tf.matrix_set_diag(kern, tf.matrix_diag_part(kern) + jitter)
