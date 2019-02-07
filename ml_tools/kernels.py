db440a6e5ad38720f1cb79ee8f7069b3ca25980c
Martin Ingram <ingramm@student.unimelb.edu.au>
2019-02-07 17:36:36 +1100

    Added a number of kernels

--- ml_tools/kernels.py
+++ ml_tools/kernels.py
@@ -100,6 +100,17 @@ def matern_kernel_32(x1, x2, alpha, rho, jitter=1e-5):
     return kernel
 
 
+def matern_kernel_12(x1, x2, alpha, rho, jitter=1e-5):
+
+    r_sq = compute_weighted_square_distances(x1, x2, rho)
+    r = np.sqrt(r_sq)
+
+    kernel = alpha**2 * np.exp(-r)
+    kernel = add_jitter(kernel, jitter)
+
+    return kernel
+
+
 def brownian_kernel_1d(x1, x2, alpha, jitter=1e-5):
 
     assert(x1.shape[1] == 1 and x2.shape[1] == 1)
@@ -107,8 +118,42 @@ def brownian_kernel_1d(x1, x2, alpha, jitter=1e-5):
     variance = alpha ** 2
 
     kernel = variance * np.where(np.sign(x1) == np.sign(x2.T),
-        np.fmin(np.abs(x1), np.abs(x2.T)), 0.)
+                                 np.fmin(np.abs(x1), np.abs(x2.T)), 0.)
 
     kernel = add_jitter(kernel, jitter)
 
     return kernel
+
+
+def mlp_kernel(x1, x2, variance, weight_variance, bias_variance, jitter=1e-5):
+
+    four_over_tau = 2. / np.pi
+
+    def comp_prod(x1, x2=None):
+
+        if x2 is None:
+            return ((np.square(x1) * weight_variance).sum(axis=1) +
+                    bias_variance)
+        else:
+            return (x1 * weight_variance).dot(x2.T) + bias_variance
+
+    x1_denom = np.sqrt(comp_prod(x1) + 1.)
+    x2_denom = np.sqrt(comp_prod(x2) + 1.)
+    xtx = comp_prod(x1, x2) / x1_denom[:, None] / x2_denom[None, :]
+    kern = variance * four_over_tau * np.arcsin(xtx)
+    kern = add_jitter(kern, jitter)
+
+    return kern
+
+
+def rq_kernel(x1, x2, variance, lscales, alpha, jitter=1e-5):
+
+    dists = compute_weighted_square_distances(x1, x2, lscales)
+
+    # Divide by alpha
+    divided = dists / alpha
+    result = (1 + divided)**(-alpha)
+    result = variance * result
+    result = add_jitter(result, jitter)
+
+    return result
