86853c664c0dc936a06af70c11cfd2eb7fc13d52
Martin Ingram <ingramm@student.unimelb.edu.au>
2019-02-28 12:22:16 +1100

    Added the Newton optimisation

---
+++ ml_tools/optimisation.py
@@ -0,0 +1,41 @@
+import numpy
+from sksparse.cholmod import cholesky
+
+
+def cholmod_solve(hess, jac):
+
+    cur_chol = cholesky(hess)
+    sol = cur_chol.solve_A(jac)
+
+    return sol
+
+
+def newton_optimise(start_f, fun, jac, hess, solve_fun=cholmod_solve):
+
+    f = start_f
+
+    n_iter = 0
+
+    # Write a Newton routine
+    difference = 1.
+
+    while difference > 1e-5:
+
+        cur_hess = hess(f)
+        cur_jac = jac(f)
+
+        sol = solve_fun(cur_hess, cur_jac)
+
+        new_f = f - sol
+        difference = numpy.linalg.norm(f - new_f)
+
+        f = new_f
+
+        n_iter = n_iter + 1
+
+        if n_iter % 10 == 0:
+            print(f"On iteration {n_iter}. Difference is {difference}")
+
+    print(f"Converged after {n_iter} iterations.")
+
+    return f
