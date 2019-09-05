03879e2207526ae82b3ffda93e7bcfd9f4e5ee86
Martin Ingram <ingramm@student.unimelb.edu.au>
2019-09-05 13:14:02 +1000

    Added plenty of evaluation tools & some kernels for autograd GPs

--- ml_tools/autograd_kernels.py
+++ ml_tools/autograd_kernels.py
@@ -3,11 +3,31 @@ import autograd.numpy as np
 
 def ard_rbf_kernel_efficient(x1, x2, alpha, rho, jitter=1e-5):
 
+    combined = compute_weighted_square_distances(x1, x2, rho)
+    kernel = alpha**2 * np.exp(-0.5 * combined)
+    kernel = add_jitter(kernel, jitter)
+
+    return kernel
+
+
+def matern_kernel_32(x1, x2, alpha, rho, jitter=1e-5):
+
+    r_sq = compute_weighted_square_distances(x1, x2, rho)
+    r = np.sqrt(r_sq)
+
+    kernel = alpha ** 2 * (1 + np.sqrt(3) * r) * np.exp(-np.sqrt(3) * r)
+    kernel = add_jitter(kernel, jitter)
+
+    return kernel
+
+
+def compute_weighted_square_distances(x1, x2, rho):
+
     z1 = x1 / np.expand_dims(rho, axis=0)
     z2 = x2 / np.expand_dims(rho, axis=0)
 
     # Matrix part
-    cross_contrib = -2 * np.matmul(z1, z2.T)
+    cross_contrib = -2 * np.dot(z1, z2.T)
 
     # Other bits
     z1_sq = np.sum(z1**2, axis=1)
@@ -17,13 +37,15 @@ def ard_rbf_kernel_efficient(x1, x2, alpha, rho, jitter=1e-5):
     combined = (np.expand_dims(z1_sq, axis=1) + cross_contrib +
                 np.expand_dims(z2_sq, axis=0))
 
-    kernel = alpha**2 * np.exp(-0.5 * combined)
+    return combined
+
+
+def add_jitter(kernel, jitter=1e-5):
 
     # Add the jitter
     diag_indices = np.diag_indices(np.min(kernel.shape[:2]))
     to_add = np.zeros_like(kernel)
     to_add[diag_indices] += jitter
-
     kernel = kernel + to_add
 
     return kernel
