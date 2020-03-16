22c99b934a74f0b89d0f497b150d92b65a452855
Martin Ingram <ingramm@student.unimelb.edu.au>
2020-03-16 19:19:11 +1100

    Added a function to remove correlated variables from numpy arrays

--- ml_tools/modelling.py
+++ ml_tools/modelling.py
@@ -7,14 +7,22 @@ def remove_correlated_variables(covariate_df, corr_threshold=0.95):
     # correlation above the threshold are dropped.
     covariate_names = covariate_df.columns
 
-    corrs = np.corrcoef(covariate_df.values.T)
-    corrs = np.tril(corrs, -1)
+    to_keep = remove_correlated_variables_array(covariate_df.values.T)
 
-    abs_corr = np.abs(corrs)
-    highly_correlated = np.where(abs_corr > 0.95)
-    to_drop = highly_correlated[0]
-
-    to_keep = np.setdiff1d(np.arange(len(covariate_names)), to_drop)
     to_keep_names = np.array(covariate_names)[to_keep]
 
     return to_keep_names
+
+
+def remove_correlated_variables_array(design_matrix, corr_threshold=0.95):
+
+    corrs = np.corrcoef(design_matrix.T)
+    corrs = np.tril(corrs, -1)
+
+    abs_corr = np.abs(corrs)
+    highly_correlated = np.where(abs_corr > corr_threshold)
+    to_drop = highly_correlated[0]
+
+    to_keep = np.setdiff1d(np.arange(design_matrix.shape[1]), to_drop)
+
+    return to_keep
